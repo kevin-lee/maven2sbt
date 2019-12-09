daf810510430013c6bc55001a3909f0b07abc92b
Kevin Lee <kevin.code@kevinlee.io>
2019-12-10 01:42:41 +1100

    Close #46 - Add output file

--- core/src/main/scala/maven2sbt/core/Maven2SbtError.scala
+++ core/src/main/scala/maven2sbt/core/Maven2SbtError.scala
@@ -12,16 +12,21 @@ object Maven2SbtError {
 
   final case class PomFileNotExist(pomFile: File) extends Maven2SbtError
   final case object NoPomInputStream extends Maven2SbtError
+  final case class OutputFileAlreadyExist(output: File) extends Maven2SbtError
 
   def pomFileNotExist(pomFile: File): Maven2SbtError = PomFileNotExist(pomFile)
   def noPomInputStream: Maven2SbtError = NoPomInputStream
+  def outputFileAlreadyExist(output: File): Maven2SbtError = OutputFileAlreadyExist(output)
 
   def render(maven2SbtError: Maven2SbtError): String = maven2SbtError match {
     case PomFileNotExist(pomFile) =>
-      s"No pom file found at ${pomFile.getCanonicalPath}."
+      s"No pom file found at ${pomFile.getCanonicalPath}"
 
     case NoPomInputStream =>
       s"No pom InputStream. It is null."
+
+    case OutputFileAlreadyExist(output) =>
+      s"Output file already exists at ${output.getCanonicalPath}"
   }
 
 }
