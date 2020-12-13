4476ae67799cab18ed8422fdb9c6dfeae3290060
Kevin Lee <kevin.code@kevinlee.io>
2020-12-13 19:56:52 +1100

    Removed: unused methods

--- core/src/main/scala/maven2sbt/core/MavenProperty.scala
+++ core/src/main/scala/maven2sbt/core/MavenProperty.scala
@@ -17,11 +17,6 @@ object MavenProperty {
     if !label.startsWith("#PCDATA")
   } yield MavenProperty(label, property.text)
 
-  // TODO: Remove it. It's no longer in use in favor of maven2sbt.core.BuildSbt.Prop.render.
-  @deprecated(message = "Use maven2sbt.core.BuildSbt.Prop.render() instead", since = "1.0.0")
-  def render(property: MavenProperty): String =
-    s"""val ${dotHyphenSeparatedToCamelCase(property.key)} = "${property.value}""""
-
   def findPropertyName(name: String): Option[String] = name match {
     case propertyUsagePattern(value) => Some(value.trim)
     case _ => None
