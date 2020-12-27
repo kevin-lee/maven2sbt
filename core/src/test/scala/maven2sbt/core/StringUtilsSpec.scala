fb6de76d442bbf5a55a1822b6c645e8336dd2220
Kevin Lee <kevin.code@kevinlee.io>
2020-12-27 21:11:57 +1100

    Issue #149 - clean up: names in tests

--- core/src/test/scala/maven2sbt/core/StringUtilsSpec.scala
+++ core/src/test/scala/maven2sbt/core/StringUtilsSpec.scala
@@ -11,35 +11,55 @@ import cats.syntax.all._
   */
 object StringUtilsSpec extends Properties {
   override def tests: List[Test] = List(
-    property("test indent", testIndent)
-  , property("test dotSeparatedToCamelCase (a single name)", testDotSeparatedToCamelCase1)
-  , property("test dotSeparatedToCamelCase (two or more names)", testDotSeparatedToCamelCaseMore)
+    property("test indent", testIndent),
+    property(
+      "test capitalizeAfterIgnoringNonAlphaNumUnderscore (a single name)",
+      testCapitalizeAfterIgnoringNonAlphaNumUnderscore1
+    ),
+    property(
+      "test capitalizeAfterIgnoringNonAlphaNumUnderscore (two or more names)",
+      testCapitalizeAfterIgnoringNonAlphaNumUnderscoreMore
+    )
   )
 
-  def testIndent: Property = for {
-    indentSize <- Gen.int(Range.linear(0, 50)).log("indentSize")
-  } yield {
-    val actual = StringUtils.indent(indentSize)
-    actual.length ==== indentSize and Result.assert(actual.trim.isEmpty)
-  }
+  def testIndent: Property =
+    for {
+      indentSize <- Gen.int(Range.linear(0, 50)).log("indentSize")
+    } yield {
+      val actual = StringUtils.indent(indentSize)
+      actual.length ==== indentSize and Result.assert(actual.trim.isEmpty)
+    }
 
-  def testDotSeparatedToCamelCase1: Property = for {
-    name <- Gen.string(Gens.genCharByRange(TestUtils.ExpectedLetters), Range.linear(1, 10)).log("name")
-  } yield {
-    val expected = name.headOption.map { c =>
-      if (c.isUpper || c.isLower || c === '_')
-        c.toString
-      else
-        s"_${c.toString}"
-    }.getOrElse("") + name.drop(1)
-    val actual = StringUtils.capitalizeAfterIgnoringNonAlphaNumUnderscore(name)
-    actual ==== expected
-  }
-  def testDotSeparatedToCamelCaseMore: Property = for {
-    (mavenPropName, propName) <- Gens.genMavenPropertyNameWithPropNamePair.log("(mavenPropName, propName)")
-  } yield {
-    val actual = StringUtils.capitalizeAfterIgnoringNonAlphaNumUnderscore(mavenPropName.name)
-    val expected = propName.propName
-    actual ==== expected
-  }
+  def testCapitalizeAfterIgnoringNonAlphaNumUnderscore1: Property =
+    for {
+      name <- Gen
+        .string(
+          Gens.genCharByRange(TestUtils.ExpectedLetters),
+          Range.linear(1, 10)
+        )
+        .log("name")
+    } yield {
+      val expected = name.headOption
+        .map { c =>
+          if (c.isUpper || c.isLower || c === '_')
+            c.toString
+          else
+            s"_${c.toString}"
+        }
+        .getOrElse("") + name.drop(1)
+      val actual =
+        StringUtils.capitalizeAfterIgnoringNonAlphaNumUnderscore(name)
+      actual ==== expected
+    }
+  def testCapitalizeAfterIgnoringNonAlphaNumUnderscoreMore: Property =
+    for {
+      (mavenPropName, propName) <- Gens.genMavenPropertyNameWithPropNamePair
+        .log("(mavenPropName, propName)")
+    } yield {
+      val actual = StringUtils.capitalizeAfterIgnoringNonAlphaNumUnderscore(
+        mavenPropName.name
+      )
+      val expected = propName.propName
+      actual ==== expected
+    }
 }
