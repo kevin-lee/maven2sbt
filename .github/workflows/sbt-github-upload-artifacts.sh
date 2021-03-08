c52e5a4e77d7ef73dc832fc64c5dd652d78042f0
Kevin Lee <kevin.code@kevinlee.io>
2021-03-09 00:45:05 +1100

    Added: manual upload artifacts to GitHub Release

---
+++ .github/workflows/sbt-github-upload-artifacts.sh
@@ -0,0 +1,54 @@
+#!/bin/bash -e
+
+set -x
+
+if [ -z "$1" ]
+  then
+    echo "Missing parameters. Please enter the [Scala version]."
+    echo "sbt-build.sh 2.13.4"
+    exit 1
+else
+
+  scala_version=$1
+  echo "============================================"
+  echo "Publish projects"
+  echo "--------------------------------------------"
+  echo ""
+  echo "mkdir -p dotty-docs"
+  mkdir -p dotty-docs
+  export SOURCE_DATE_EPOCH=$(date +%s)
+  echo "SOURCE_DATE_EPOCH=$SOURCE_DATE_EPOCH"
+
+  if [ "$2" == "cli" ]
+  then
+    echo ""
+    sbt \
+      -J-Xmx2048m \
+      ++${scala_version}! \
+      -v \
+      clean \
+      test \
+      packagedArtifacts \
+      universal:packageBin \
+      universal:packageZipTarball \
+      debian:packageBin \
+      devOopsGitHubReleaseUploadArtifacts
+
+      ls -ld cli/target/maven2sbt-cli*
+      ls -ld cli/target/universal/maven2sbt-cli*
+  else
+    sbt \
+      -J-Xmx2048m \
+      ++${scala_version}! \
+      - v \
+      clean \
+      test \
+      packagedArtifacts \
+      devOopsGitHubReleaseUploadArtifacts
+  fi
+
+echo "============================================"
+echo "Publish projects: Done"
+echo "============================================"
+
+fi
