b9139ab05aff06d24f28c95ed12f49cab5722355
Kevin Lee <kevin.code@kevinlee.io>
2022-04-19 22:02:47 +1000

    Close #294 - Upgrade WartRemover to 3.0.2

--- project/SbtProjectInfo.scala
+++ project/SbtProjectInfo.scala
@@ -6,8 +6,6 @@ object SbtProjectInfo {
   def commonWarts(scalaVersion: String): Seq[wartremover.Wart] =
     if (scalaVersion.startsWith("2.10"))
       Seq.empty
-    else if (scalaVersion.startsWith("3."))
-      Warts.allBut(Wart.ImplicitConversion, Wart.ImplicitParameter, Wart.Null)
     else
       Warts.allBut(Wart.ImplicitConversion, Wart.ImplicitParameter)
 
